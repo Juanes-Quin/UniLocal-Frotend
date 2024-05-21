a913dc0e31a749b0396e6258e65910644a7c5137
michael <michael125lol@gmail.com>
2024-05-21 17:03:48 -0500

    cambiamos ya aparece el login era poner provideHttpClient() en el app congigs

---
+++ src/app/componentes/crear-negocio/crear-negocio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-negocio',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-negocio.component.html',
+  styleUrl: './crear-negocio.component.css'
+})
+export class CrearNegocioComponent {
+
+}
