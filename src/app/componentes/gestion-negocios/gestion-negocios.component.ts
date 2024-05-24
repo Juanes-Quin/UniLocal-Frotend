3118962fe963a757d72bc112cf97935d168caca9
IsmeniaIMGO <ismeniam.guevarao@uqvirtual.edu.co>
2024-05-23 23:08:59 -0500

    modificacion en vista mis negocios

--- src/app/componentes/gestion-negocios/gestion-negocios.component.ts
+++ src/app/componentes/gestion-negocios/gestion-negocios.component.ts
@@ -13,16 +13,13 @@ import { TokenService } from '../../servicios/token.service';
   templateUrl: './gestion-negocios.component.html',
   styleUrl: './gestion-negocios.component.css'
 })
+
 export class GestionNegociosComponent {
 
   negocios: ItemNegocioDTO[];
-  seleccionados: ItemNegocioDTO[];
-  textoBtnEliminar: string;
 
 constructor(private negocioService: NegociosService, private publicoService: PublicoService, private tokenService: TokenService) {
 this.negocios = [];
-this.seleccionados = [];
-this.textoBtnEliminar = "Eliminar";
 this.listarNegocios();
 }
 
@@ -30,36 +27,6 @@ this.listarNegocios();
 public listarNegocios(){
 this.negocios = this.negocioService.listar();
 }*/
-
-public seleccionar(producto: ItemNegocioDTO, estado: boolean) {
-  if (estado) {
-  this.seleccionados.push(producto);
-  } else {
-    this.seleccionados.splice(this.seleccionados.indexOf(producto), 1);
-  }
-  this.actualizarMensaje();
-}
-private actualizarMensaje() {
-const tam = this.seleccionados.length;
-if (tam != 0) {
-if (tam == 1) {
-this.textoBtnEliminar = "1 elemento";
-} else {
-this.textoBtnEliminar = tam + " elementos";
-}
-} else {
-this.textoBtnEliminar = "";
-}
-}
-
-public borrarNegocios() {
-  this.seleccionados.forEach(n => {
-  this.negocioService.eliminar(n.codigoNegocio);
-  this.negocios = this.negocios.filter(negocio => negocio.codigoNegocio !== n.codigoNegocio);
-  });
-  this.seleccionados = [];
-  this.actualizarMensaje();
-}
 
 public listarNegocios(){
   const codigoCliente = this.tokenService.getCodigo();
