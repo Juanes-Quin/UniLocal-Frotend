241aca323d9016b7157bc44c8b9bb422d5354807
IsmeniaIMGO <ismeniam.guevarao@uqvirtual.edu.co>
2024-05-26 20:33:34 -0500

    agregar metodos de busquedas de negocio a registro.component

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -8,6 +8,11 @@ import { AuthService } from '../../servicios/auth.service';
 import { Alerta } from '../../model/alerta';
 import { ImagenService } from '../../servicios/imagen.service';
 import { Route, RouterModule } from '@angular/router';
+import {ClienteService} from "../../servicios/cliente.service";
+import {BusquedaNombreDTO} from "../../dto/BusquedaNombreDTO";
+import {CategoriaNegocioDTO} from "../../dto/CategoriaNegocioDTO";
+import {BusquedaDistanciaDTO} from "../../dto/BusquedaDistanciaDTO";
+import {EstadoNegocioDTO} from "../../dto/EstadoNegocioDTO";
 
 @Component({
   selector: 'app-registro',
@@ -19,18 +24,33 @@ import { Route, RouterModule } from '@angular/router';
 
 export class RegistroComponent {
   registroClienteDTO: RegistroClienteDTO;
+  busquedaNombreDTO: BusquedaNombreDTO;
+  categoriaNegocio: CategoriaNegocioDTO;
+  busquedaDistanciaDTO: BusquedaDistanciaDTO;
+  estadoNegocio: EstadoNegocioDTO;
+
   ciudades: string[];
+  categorias: string[];
   archivos!: FileList;
   alerta!: Alerta;
 
   constructor(
     private publicoService: PublicoService,
+    private clienteService: ClienteService,
     private authService: AuthService,
     private imagenService: ImagenService
   ) {
     this.registroClienteDTO = new RegistroClienteDTO();
     this.ciudades = [];
     this.cargarCiudades();
+
+    this.categorias = [];
+    this.cargarCategorias();
+
+    this.busquedaNombreDTO = new BusquedaNombreDTO();
+    this.categoriaNegocio = new CategoriaNegocioDTO();
+    this.busquedaDistanciaDTO = new BusquedaDistanciaDTO();
+    this.estadoNegocio = new EstadoNegocioDTO();
   }
 
   /**
@@ -78,6 +98,15 @@ export class RegistroComponent {
         console.log("Error al cargar las ciudades");
       }
     });
+
+    this.clienteService.listarCiudades().subscribe({
+      next: (data) => {
+        this.ciudades = data.respuesta;
+      },
+      error: (error) => {
+        console.log("Error al cargar las ciudades");
+      }
+    });
   }
 
   /**
@@ -107,4 +136,107 @@ export class RegistroComponent {
       this.alerta = new Alerta("Debe seleccionar una imagen y subirla", "danger");
     }
   }
+
+  private cargarCategorias() {
+    this.publicoService.listarCategoriaNegocio().subscribe({
+      next: (data) => {
+        this.categorias = data.respuesta;
+      },
+      error: (error) => {
+        console.log("Error al cargar las categorias");
+      }
+    });
+
+    this.clienteService.listarCategoriaNegocio().subscribe({
+      next: (data) => {
+        this.categorias = data.respuesta;
+      },
+      error: (error) => {
+        console.log("Error al cargar las categorias");
+      }
+    });
+
+    }
+
+    public buscarNegocioNombre(busquedaNombreDTO: BusquedaNombreDTO) {
+      this.publicoService.buscarNegocioNombre(busquedaNombreDTO).subscribe({
+        next: (data) => {
+          this.alerta = new Alerta(data.respuesta, "success");
+        },
+        error: (error) => {
+          this.alerta = new Alerta("Error al buscar negocio por nombre", "danger");
+        }
+      });
+
+      this.clienteService.buscarNegocioNombre(busquedaNombreDTO).subscribe({
+        next: (data) => {
+          this.alerta = new Alerta(data.respuesta, "success");
+        },
+        error: (error) => {
+          this.alerta = new Alerta("Error al buscar negocio por nombre", "danger");
+        }
+      });
+
+  }
+
+    public buscarNegocioCategoria(categoriaNegocio: CategoriaNegocioDTO) {
+      this.publicoService.buscarNegocioCategoria(categoriaNegocio).subscribe({
+        next: (data) => {
+          this.alerta = new Alerta(data.respuesta, "success");
+        },
+        error: (error) => {
+          this.alerta = new Alerta("Error al buscar negocio por categoria", "danger");
+        }
+      });
+
+      this.clienteService.buscarNegocioCategoria(categoriaNegocio).subscribe({
+        next: (data) => {
+          this.alerta = new Alerta(data.respuesta, "success");
+        },
+        error: (error) => {
+          this.alerta = new Alerta("Error al buscar negocio por categoria", "danger");
+        }
+      });
+    }
+
+    public buscarNegocioDistancia(busquedaDistanciaDTO: BusquedaDistanciaDTO) {
+      this.publicoService.buscarNegocioDistancia(busquedaDistanciaDTO).subscribe({
+        next: (data) => {
+          this.alerta = new Alerta(data.respuesta, "success");
+        },
+        error: (error) => {
+          this.alerta = new Alerta("Error al buscar negocio por distancia", "danger");
+        }
+      });
+
+      this.clienteService.buscarNegocioDistancia(busquedaDistanciaDTO).subscribe({
+        next: (data) => {
+          this.alerta = new Alerta(data.respuesta, "success");
+        },
+        error: (error) => {
+          this.alerta = new Alerta("Error al buscar negocio por distancia", "danger");
+        }
+      });
+    }
+
+    public filtrarPorEstado(estadoNegocio: EstadoNegocioDTO) {
+      this.publicoService.filtrarPorEstado(estadoNegocio).subscribe({
+        next: (data) => {
+          this.alerta = new Alerta(data.respuesta, "success");
+        },
+        error: (error) => {
+          this.alerta = new Alerta("Error al filtrar negocio por estado", "danger");
+        }
+      });
+
+      this.clienteService.filtrarPorEstado(estadoNegocio).subscribe({
+        next: (data) => {
+          this.alerta = new Alerta(data.respuesta, "success");
+        },
+        error: (error) => {
+          this.alerta = new Alerta("Error al filtrar negocio por estado", "danger");
+        }
+      });
+    }
+
 }
